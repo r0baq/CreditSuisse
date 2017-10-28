79f563aa273d745cbfa5bb96df90a7ead98f3ad3
Robert Mroczka <mroczka.robert@gmail.com>
2017-10-28 20:16:19 +0200

    [TEST] More tests.

--- src/test/java/org/mroczkarobert/creditsuisse/service/TestValidationService.java
+++ src/test/java/org/mroczkarobert/creditsuisse/service/TestValidationService.java
@@ -14,6 +14,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mroczkarobert.creditsuisse.transport.ErrorTrade;
 import org.mroczkarobert.creditsuisse.transport.Trade;
+import org.mroczkarobert.creditsuisse.type.ErrorCode;
 import org.mroczkarobert.creditsuisse.util.InvalidCurrencyException;
 import org.mroczkarobert.creditsuisse.validator.SpotForwardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,7 +41,7 @@ public class TestValidationService {
 	}
 	
 	@Test
-	public void shouldEndWithoutErrors() throws InvalidCurrencyException {
+	public void shouldSpotEndWithoutErrors() throws InvalidCurrencyException {
 		//given
 		Trade trade = new Trade();
 		trade.setCustomer("PLUTO1");
@@ -64,4 +65,192 @@ public class TestValidationService {
 		//then
 		assertThat(result.getErrors()).isEmpty();
 	}
+	
+	@Test
+	public void shouldReportInvalidCurrency() throws InvalidCurrencyException {
+		//given
+		Trade trade = new Trade();
+		trade.setCustomer("PLUTO1");
+		trade.setCcyPair("XXXUSD");
+		trade.setType("Spot");
+		trade.setDirection("BUY");
+		trade.setTradeDate(LocalDate.parse("2016-08-11"));
+		trade.setAmount1(new BigDecimal("1000000.00"));
+		trade.setAmount2(new BigDecimal("1120000.00"));
+		trade.setRate(new BigDecimal("1.12"));
+		trade.setValueDate(LocalDate.parse("2016-08-15"));
+		trade.setLegalEntity("CS Zurich");
+		trade.setTrader("Johann Baumfiddler");
+		
+		Mockito.when(currencyRemoteService.isWorkingDay(LocalDate.parse("2016-08-15"), "XXX")).thenThrow(new InvalidCurrencyException());
+		Mockito.when(currencyRemoteService.isWorkingDay(LocalDate.parse("2016-08-15"), "USD")).thenReturn(true);
+		
+		//when
+		ErrorTrade result = validationService.validate(trade);
+		
+		//then
+		assertThat(result.getErrors().iterator().next().getErrorCode()).isEqualTo(ErrorCode.INVALID_CURRENCY_1);
+	}
+	
+	@Test
+	public void shouldReportEmptyTradeDate() throws InvalidCurrencyException {
+		//given
+		Trade trade = new Trade();
+		trade.setCustomer("PLUTO1");
+		trade.setCcyPair("EURUSD");
+		trade.setType("Spot");
+		trade.setDirection("BUY");
+		trade.setAmount1(new BigDecimal("1000000.00"));
+		trade.setAmount2(new BigDecimal("1120000.00"));
+		trade.setRate(new BigDecimal("1.12"));
+		trade.setValueDate(LocalDate.parse("2016-08-15"));
+		trade.setLegalEntity("CS Zurich");
+		trade.setTrader("Johann Baumfiddler");
+		
+		Mockito.when(currencyRemoteService.isWorkingDay(LocalDate.parse("2016-08-15"), "EUR")).thenReturn(true);
+		Mockito.when(currencyRemoteService.isWorkingDay(LocalDate.parse("2016-08-15"), "USD")).thenReturn(true);
+		
+		//when
+		ErrorTrade result = validationService.validate(trade);
+		
+		//then
+		assertThat(result.getErrors().iterator().next().getErrorCode()).isEqualTo(ErrorCode.EMPTY_TRADE_DATE);
+	}
+	
+	@Test
+	public void shouldReportValueDateBeforeTradeDate() throws InvalidCurrencyException {
+		//given
+		Trade trade = new Trade();
+		trade.setCustomer("PLUTO1");
+		trade.setCcyPair("EURUSD");
+		trade.setType("Spot");
+		trade.setDirection("BUY");
+		trade.setTradeDate(LocalDate.parse("2016-08-11"));
+		trade.setAmount1(new BigDecimal("1000000.00"));
+		trade.setAmount2(new BigDecimal("1120000.00"));
+		trade.setRate(new BigDecimal("1.12"));
+		trade.setValueDate(LocalDate.parse("2016-08-10"));
+		trade.setLegalEntity("CS Zurich");
+		trade.setTrader("Johann Baumfiddler");
+		
+		Mockito.when(currencyRemoteService.isWorkingDay(LocalDate.parse("2016-08-10"), "EUR")).thenReturn(true);
+		Mockito.when(currencyRemoteService.isWorkingDay(LocalDate.parse("2016-08-10"), "USD")).thenReturn(true);
+		
+		//when
+		ErrorTrade result = validationService.validate(trade);
+		
+		//then
+		assertThat(result.getErrors().iterator().next().getErrorCode()).isEqualTo(ErrorCode.VALUE_DATE_BEFORE_TRADE_DATE);
+	}
+	
+	@Test
+	public void shouldReportUnknownCustomer() throws InvalidCurrencyException {
+		//given
+		Trade trade = new Trade();
+		trade.setCustomer("PLUTO3");
+		trade.setCcyPair("EURUSD");
+		trade.setType("Spot");
+		trade.setDirection("BUY");
+		trade.setTradeDate(LocalDate.parse("2016-08-11"));
+		trade.setAmount1(new BigDecimal("1000000.00"));
+		trade.setAmount2(new BigDecimal("1120000.00"));
+		trade.setRate(new BigDecimal("1.12"));
+		trade.setValueDate(LocalDate.parse("2016-08-15"));
+		trade.setLegalEntity("CS Zurich");
+		trade.setTrader("Johann Baumfiddler");
+		
+		Mockito.when(currencyRemoteService.isWorkingDay(LocalDate.parse("2016-08-15"), "EUR")).thenReturn(true);
+		Mockito.when(currencyRemoteService.isWorkingDay(LocalDate.parse("2016-08-15"), "USD")).thenReturn(true);
+		
+		//when
+		ErrorTrade result = validationService.validate(trade);
+		
+		//then
+		assertThat(result.getErrors().iterator().next().getErrorCode()).isEqualTo(ErrorCode.UNKNOWN_CUSTOMER);
+	}
+	
+	@Test
+	public void shouldReportUnkonwLegalEntity() throws InvalidCurrencyException {
+		//given
+		Trade trade = new Trade();
+		trade.setCustomer("PLUTO1");
+		trade.setCcyPair("EURUSD");
+		trade.setType("Spot");
+		trade.setDirection("BUY");
+		trade.setTradeDate(LocalDate.parse("2016-08-11"));
+		trade.setAmount1(new BigDecimal("1000000.00"));
+		trade.setAmount2(new BigDecimal("1120000.00"));
+		trade.setRate(new BigDecimal("1.12"));
+		trade.setValueDate(LocalDate.parse("2016-08-15"));
+		trade.setLegalEntity("CS Zurich XXX");
+		trade.setTrader("Johann Baumfiddler");
+		
+		Mockito.when(currencyRemoteService.isWorkingDay(LocalDate.parse("2016-08-15"), "EUR")).thenReturn(true);
+		Mockito.when(currencyRemoteService.isWorkingDay(LocalDate.parse("2016-08-15"), "USD")).thenReturn(true);
+		
+		//when
+		ErrorTrade result = validationService.validate(trade);
+		
+		//then
+		assertThat(result.getErrors().iterator().next().getErrorCode()).isEqualTo(ErrorCode.UNKNOWN_LEGAL_ENTITY);
+	}
+	
+	@Test
+	public void shouldReportTypeNotAvalibleForDate() throws InvalidCurrencyException {
+		//given
+		Trade trade = new Trade();
+		trade.setCustomer("PLUTO1");
+		trade.setCcyPair("EURUSD");
+		trade.setType("Spot");
+		trade.setDirection("BUY");
+		trade.setTradeDate(LocalDate.parse("2010-08-11"));
+		trade.setAmount1(new BigDecimal("1000000.00"));
+		trade.setAmount2(new BigDecimal("1120000.00"));
+		trade.setRate(new BigDecimal("1.12"));
+		trade.setValueDate(LocalDate.parse("2010-12-31"));
+		trade.setLegalEntity("CS Zurich");
+		trade.setTrader("Johann Baumfiddler");
+		
+		Mockito.when(currencyRemoteService.isWorkingDay(LocalDate.parse("2010-12-31"), "EUR")).thenReturn(true);
+		Mockito.when(currencyRemoteService.isWorkingDay(LocalDate.parse("2010-12-31"), "USD")).thenReturn(true);
+		
+		//when
+		ErrorTrade result = validationService.validate(trade);
+		
+		//then
+		assertThat(result.getErrors().iterator().next().getErrorCode()).isEqualTo(ErrorCode.TYPE_NOT_AVAILABLE_FOR_DATE);
+	}
+	
+	@Test
+	public void shouldOptionEndWithoutErrors() throws InvalidCurrencyException {
+		//given
+		Trade trade = new Trade();
+		trade.setCustomer("PLUTO1");
+		trade.setCcyPair("EURUSD");
+		trade.setType("VanillaOption");
+		trade.setStyle("EUROPEAN");
+		trade.setDirection("BUY");
+		trade.setStrategy("CALL");
+		trade.setTradeDate(LocalDate.parse("2016-08-11"));
+		trade.setAmount1(new BigDecimal("1000000.00"));
+		trade.setAmount2(new BigDecimal("1120000.00"));
+		trade.setRate(new BigDecimal("1.12"));
+		trade.setDeliveryDate(LocalDate.parse("2016-08-22"));
+		trade.setExpiryDate(LocalDate.parse("2016-08-19"));
+		trade.setPayCcy("USD");
+		trade.setPremium(new BigDecimal("0.20"));
+		trade.setPremiumCcy("USD");
+		trade.setPremiumType("%USD");
+		trade.setPremiumDate(LocalDate.parse("2016-08-12"));
+		trade.setLegalEntity("CS Zurich");
+		trade.setTrader("Johann Baumfiddler");
+		
+		//when
+		ErrorTrade result = validationService.validate(trade);
+		
+		//then
+		assertThat(result.getErrors()).isEmpty();
+	}
+	
+	//RMR więcej niż jeden błąd
 }
