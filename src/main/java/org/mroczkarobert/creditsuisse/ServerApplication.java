1bcb6cc80109771c00f5299f535f2323f475f1e0
Robert Mroczka <mroczka.robert@gmail.com>
2017-10-29 12:53:22 +0100

    Graceful shutdown support.

--- src/main/java/org/mroczkarobert/creditsuisse/ServerApplication.java
+++ src/main/java/org/mroczkarobert/creditsuisse/ServerApplication.java
@@ -1,8 +1,13 @@
 package org.mroczkarobert.creditsuisse;
 
+import org.mroczkarobert.creditsuisse.util.GracefulShutdownCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
@@ -12,5 +17,24 @@ public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Bean
+	public GracefulShutdownCustomizer gracefulShutdownCustomizer() {
+		return new GracefulShutdownCustomizer();
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer tomcatCustomizer() {
+		return new EmbeddedServletContainerCustomizer() {
+
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				if (container instanceof TomcatEmbeddedServletContainerFactory) {
+					((TomcatEmbeddedServletContainerFactory) container).addConnectorCustomizers(gracefulShutdownCustomizer());
+				}
+
+			}
+		};
 	}
 }
---
+++ src/main/java/org/mroczkarobert/creditsuisse/util/GracefulShutdownCustomizer.java
@@ -0,0 +1,43 @@
+package org.mroczkarobert.creditsuisse.util;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+
+public class GracefulShutdownCustomizer implements TomcatConnectorCustomizer, ApplicationListener<ContextClosedEvent> {
+
+	private Logger log = LogManager.getLogger();
+
+	private volatile Connector connector;
+
+	@Override
+	public void customize(Connector connector) {
+		this.connector = connector;
+	}
+
+	@Override
+	public void onApplicationEvent(ContextClosedEvent event) {
+		connector.pause();
+		Executor executor = connector.getProtocolHandler().getExecutor();
+		
+		if (executor instanceof ThreadPoolExecutor) {
+			try {
+				ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
+				threadPoolExecutor.shutdown();
+				if (!threadPoolExecutor.awaitTermination(30, TimeUnit.SECONDS)) {
+					log.warn("Tomcat thread pool did not shut down gracefully within 30 seconds. Proceeding with forceful shutdown");
+				}
+				
+			} catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
+}
